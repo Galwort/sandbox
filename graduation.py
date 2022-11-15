2cdb2027d4e1a5c1980d9cabb787778569506919
Tom Gorbett <gorbetw@ccf.org>
2022-11-15 14:42:59 -0500

    working on looping through passing_scores

--- graduation.py
+++ graduation.py
@@ -1,18 +1,20 @@
 from random import uniform
+import seaborn as sns
 
 grade = 1
-passing_score = 0.9
+passing_score = 0.5
 score = 0
 curve = 0
 year = 0
 
-while grade <= 12:
-    score = uniform(curve, 1)
-    if score > passing_score:
-        grade += 1
-        curve = score - 0.5
-        year += 1
-    elif score <= passing_score:
-        year += 1
 
-print("Years to graduate: ", year)
+while passing_score < 1:
+    for x in range(1000):
+        while grade <= 12:
+            score = uniform(curve, 1)
+            if score > passing_score:
+                grade += 1
+                curve = score - 0.5
+                year += 1
+            elif score <= passing_score:
+                year += 1
