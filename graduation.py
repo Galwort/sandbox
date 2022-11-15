9de13765f202ee26bd1096a6299ff6cf8053cf6a
Tom Gorbett <gorbetw@ccf.org>
2022-11-15 14:51:38 -0500

    a little more work before my computer dies

--- graduation.py
+++ graduation.py
@@ -1,15 +1,16 @@
 from random import uniform
+import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 grade = 1
 passing_score = 0.5
 score = 0
 curve = 0
 year = 0
-
-
+report_card = pd.DataFrame(columns=["passing_score", "student", "years"])
 while passing_score < 1:
-    for x in range(1000):
+    for students in range(1000):
         while grade <= 12:
             score = uniform(curve, 1)
             if score > passing_score:
@@ -18,3 +19,5 @@ while passing_score < 1:
                 year += 1
             elif score <= passing_score:
                 year += 1
+        report_card
+    passing_score += 0.1
