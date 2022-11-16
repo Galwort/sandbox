1ca37f39a360b457b43892352635efdb5a69ef9d
Tom Gorbett <gorbetw@ccf.org>
2022-11-15 22:00:47 -0500

    got an actual csv to output yay

--- graduation.py
+++ graduation.py
@@ -9,6 +9,7 @@ score = 0
 curve = 0
 year = 0
 report_card = pd.DataFrame(columns=["passing_score", "student", "years"])
+
 while passing_score < 1:
     for students in range(1000):
         while grade <= 12:
@@ -19,5 +20,11 @@ while passing_score < 1:
                 year += 1
             elif score <= passing_score:
                 year += 1
-        report_card
+        x_df = pd.DataFrame({"passing_score": passing_score,
+                             "student": students,
+                             "years": year},
+                             index=[0])
+        report_card = pd.concat([report_card,x_df], ignore_index=True)
     passing_score += 0.1
+
+report_card.to_csv("report_card.csv")
