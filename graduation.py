0352bf9b197c4081835b43c58462e4e796425ba4
Tom Gorbett <gorbetw@ccf.org>
2022-11-19 22:46:27 -0500

    swapping out for loop with while

--- graduation.py
+++ graduation.py
@@ -3,27 +3,31 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-passing_score = 0.5
+passing_score = 50
 curve = 0
 report_card = pd.DataFrame(columns=["passing_score", "student", "years"])
 year = 0
 grade = 1
+student = 1
 
-while passing_score < 1:
-    for students in range(10):
+while passing_score < 100:
+    student = 1
+    while student < 1000:
         grade = 1
+        year = 0
         while grade <= 12:
-            score = uniform(curve, 1)
+            score = uniform(curve, 100)
             if score > passing_score:
                 grade += 1
-                curve = score - 0.5
+                curve = score - 50
                 year += 1
             elif score <= passing_score:
                 year += 1
         x_df = pd.DataFrame({"passing_score": passing_score,
-                             "student": students,
+                             "student": student,
                              "years": year},
                              index=[0])
         report_card = pd.concat([report_card,x_df], ignore_index=True)
-    passing_score += 0.1
-report_card.to_csv("report_card.csv")
+        student += 1
+    passing_score += 1
+report_card.to_csv("report_card.csv", index=False)
