843b43cf0809637fa527320ae8f5509328e9ba66
Tom Gorbett <gorbetw@ccf.org>
2022-11-20 18:12:35 -0500

    cleaning up visualization code

--- graduation.py
+++ graduation.py
@@ -19,7 +19,7 @@ while passing_score < 100:
             score = uniform(curve, 100)
             if score > passing_score:
                 grade += 1
-                curve = score - 10 * grade
+                curve = score - 50 #* grade
                 year += 1
             elif score <= passing_score:
                 year += 1
@@ -29,20 +29,7 @@ while passing_score < 100:
                              index=[0])
         report_card = pd.concat([report_card,x_df], ignore_index=True)
         student += 1
-    passing_score += 1
-
-# report_card.to_csv("report_card.csv", index=False)
-
-sns.set_style("whitegrid")
-sns.set_context("poster")
-sns.set_palette("Set2")
-sns.set_color_codes("pastel")
-sns.set(rc={'figure.figsize':(11.7,8.27)})
-sns.set(font_scale=1.5)
-sns.set_style("ticks")
-sns.set_style({"xtick.direction": "in","ytick.direction": "in"})
-sns.set_style({"xtick.major.size": 8, "ytick.major.size": 8})
-sns.set_style({"xtick.minor.size": 4, "ytick.minor.size": 4})
+    passing_score += 10
 
 sns_plot = sns.boxplot(x="passing_score", y="years", data=report_card)
 sns_plot.set(xlabel='Passing Score', ylabel='Years to Graduate')
