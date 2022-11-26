34301da9bdf6dda4b6119e45e460716e11870bd8
Tom Gorbett <gorbetw@ccf.org>
2022-11-26 16:00:41 -0500

    simplied color generation blocks

--- desktop-flashcards.py
+++ desktop-flashcards.py
@@ -4,29 +4,20 @@ from PIL import Image, ImageDraw, ImageFont
 from random import choice, randint
 from textwrap import wrap
 
-def rgb_to_hex(red, green, blue):
-    return '#%02x%02x%02x' % (red, green, blue)
+desk_rgb, card_rgb, outline_rgb, colors = [], [], [], []
 
-desk_r = randint(0,50)
-desk_g = randint(0,50)
-desk_b = randint(0,50)
+for i in range(3):
+    desk_rgb.append(randint(0, 50))
+    card_rgb.append(desk_rgb[i] + 20)
+    outline_rgb.append(desk_rgb[i] + 10)
 
-card_r = desk_r + 20
-card_g = desk_g + 20
-card_b = desk_b + 20
-
-outline_r = desk_r + 10
-outline_g = desk_g + 10
-outline_b = desk_b + 10
-
-desk_color = rgb_to_hex(desk_r, desk_g, desk_b)
-card_color = rgb_to_hex(card_r, card_g, card_b)
-outline_color = rgb_to_hex(outline_r, outline_g, outline_b)
+for x in (desk_rgb, card_rgb, outline_rgb):
+    colors.append('#%02x%02x%02x' % tuple(x))
 
 desk_x = windll.user32.GetSystemMetrics(0)
 desk_y = windll.user32.GetSystemMetrics(1)
 
-desk_im = Image.new('RGB', (desk_x, desk_y), desk_color)
+desk_im = Image.new('RGB', (desk_x, desk_y), colors[0])
 desk_draw = ImageDraw.Draw(desk_im)
 card_font = ImageFont.truetype('calibri.ttf', int(desk_y / 25))
 
@@ -37,16 +28,13 @@ line = choice(lines).strip()
 card_text = '\n'.join(wrap(line, 50))
 _, _, w, h = desk_draw.multiline_textbbox((0,0), card_text, card_font, spacing=20)
 
-card_width = 1000
-card_height = 200
-
 desk_draw.rounded_rectangle((int(desk_x / 2 - (w + 100) / 2),
                             int(desk_y / 2 - (h + 100) / 2),
                             int(desk_x / 2 + (w + 100) / 2),
                             int(desk_y / 2 + (h + 100) / 2)),
                             50,
-                            card_color,
-                            outline_color,
+                            colors[1],
+                            colors[2],
                             3)
 
 desk_draw.text(((desk_x - w)/2,
