fe58164ac5326ede1a73feec645577d6197747f5
Tom Gorbett <gorbetw@ccf.org>
2022-11-26 17:01:05 -0500

    added hexagons to the background

--- desktop-flashcards.py
+++ desktop-flashcards.py
@@ -4,22 +4,54 @@ from PIL import Image, ImageDraw, ImageFont
 from random import choice, randint
 from textwrap import wrap
 
-desk_rgb, card_rgb, outline_rgb, colors = [], [], [], []
+desk_rgb, card_rgb, outline_rgb, gon_rgb, colors, xy_map = ([] for i in range(6))
 
 for i in range(3):
     desk_rgb.append(randint(0, 50))
     card_rgb.append(desk_rgb[i] + 20)
     outline_rgb.append(desk_rgb[i] + 10)
+    gon_rgb.append(desk_rgb[i] - 10) # round to zero
 
-for x in (desk_rgb, card_rgb, outline_rgb):
-    colors.append('#%02x%02x%02x' % tuple(x))
+for i in (desk_rgb, card_rgb, outline_rgb, gon_rgb):
+    colors.append('#%02x%02x%02x' % tuple(i))
 
 desk_x = windll.user32.GetSystemMetrics(0)
 desk_y = windll.user32.GetSystemMetrics(1)
 
+x, y = 80, 0
+while x <= desk_x:
+    while y <= desk_y:
+        xy_map.append((x,y))
+        y += 140
+    x += 240
+    y = 0
+
+x, y = 200, 70
+while x <= desk_x:
+    while y <= desk_y:
+        xy_map.append((x,y))
+        y += 140
+    x += 240
+    y = 70
+
 desk_im = Image.new('RGB', (desk_x, desk_y), colors[0])
 desk_draw = ImageDraw.Draw(desk_im)
 card_font = ImageFont.truetype('calibri.ttf', int(desk_y / 25))
+
+for i in range(randint(10, 30)):
+    gon = randint(0, len(xy_map))
+    x = xy_map[gon][0]
+    y = xy_map[gon][1]
+    gon_color = colors[2] if randint(0,2) == 0 else colors[3]
+    desk_draw.polygon([
+        (x-40, y+70),
+        (x+40, y+70),
+        (x+80, y),
+        (x+40, y-70),
+        (x-40, y-70),
+        (x-80, y),
+    ], fill=gon_color)
+    i += 1
 
 with open('flashcards.txt') as f:
     lines = f.readlines()
